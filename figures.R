fa85bbf9ebe7e216de501bc110105272be7c5c29
Alex Rich <alexandersrich@gmail.com>
2014-12-09 11:25:05 -0500

    changed figure colors
    still doesn't show color differences in black and white, but now red
    maps to bad and green to good. The figure can be neatened up a bit in
    illustrator, but waiting for final verdict on colors/textures

--- figures.R
+++ figures.R
@@ -4,7 +4,7 @@ library("XML")
 library("gridExtra")
 
 theurl = "http://gureckislab.org/blog/?p=4098"
-tables = readHTMLTable(theurl)
+tables = readHTMLTable(theurl, header=F)
 
 tables = llply(tables, function(df){df$answer = sub("\u2026", "...", sub("\u2019", "'", as.character(df$V1)));
                                     df$count = as.numeric(as.character(df$V2));
@@ -24,7 +24,11 @@ benefits = benefits[order(benefits$percent),]
 combinedquestions = rbind(benefits, challenges, uses)
 
 combinedquestions = combinedquestions[combinedquestions$answer!="Other",]
+combinedquestions[c(6,10,17), "answer"] = c("I am based outside the US",
+                     "Experiments I'm interested in don't work well online",
+                     "conducting full experiments")
 combinedquestions$answer = factor(combinedquestions$answer, levels=as.character(combinedquestions$answer))
+
 
 
 features = tables[[8]]
@@ -32,8 +36,8 @@ features$answer=sub("\u00A0", " ", as.character(features$answer))
 features$psiTurk = c(1,1,0,1,1,1,0,1,1,1,0,1,2,2,2,0,0,1,0)
 
 
-pdf(file="figures/combinedquestions.pdf", width = 10, height = 10, useDingbats = FALSE)
-ggplot(combinedquestions, aes(answer, percent, fill=label)) + geom_bar() + coord_flip() + theme_classic() + xlab("") + ylab("Percent reporting challenge") + scale_y_continuous(limits=c(0,100))
+pdf(file="figures/combinedquestions2.pdf", width = 12, height = 5, useDingbats = FALSE)
+ggplot(combinedquestions, aes(answer, percent, fill=label)) + geom_bar(stat="identity") + coord_flip() + theme_classic() + xlab("") + ylab("Percent reporting challenge") + scale_y_continuous(limits=c(0,100)) + scale_fill_manual(values=c("#4daf4a", "#e41a1c", "#377eb8"), name="Response type", breaks = c("uses", "challenges", "benefits"))
 dev.off()
 
 
